8461cc2f4a72a1b19bb0559b9c157b8b697e90ab
PC6live <740687544@outlook.com>
2020-05-07 16:32:40 +0800

    add joybook

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,7 +1,36 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common');
+const webpack = require('webpack');
+
+const message = `
+// ==UserScript==
+// @name        bilibili
+// @namespace   test
+// @description 共享大会员
+// @version     0.1
+// @include     *://*.bilibili.com/*
+// @exclude     *://passport.bilibili.com/*
+// @grant       GM_cookie
+// @grant       GM.cookie
+// @grant       GM_setValue
+// @grant       GM_getValue
+// @grant       GM_deleteValue
+// @grant       unsafeWindow
+// @run-at      document-start
+// @noframes
+// ==/UserScript==
+`
 
 module.exports = merge(common, {
 	mode: 'production',
 	// devtool: 'source-map',
+	optimization: {
+		minimize: true,
+	},
+	plugins: [
+		new webpack.BannerPlugin({
+			banner: message,
+			entryOnly: true,
+		}),
+	],
 });
