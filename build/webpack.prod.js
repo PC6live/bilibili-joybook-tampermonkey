ccfc09258bd1e1b5edb85449a3d63326464502ed
PC6live <740687544@outlook.com>
2020-05-07 14:09:11 +0800

    remove build externals react

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -4,8 +4,4 @@ const common = require('./webpack.common');
 module.exports = merge(common, {
 	mode: 'production',
 	// devtool: 'source-map',
-	externals: {
-		react: 'React',
-		'react-dom': 'ReactDOM',
-	},
 });
