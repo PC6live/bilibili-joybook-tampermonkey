6a33804ae2bfb93ea35338e722f0a797b1f138c8
PC6live <740687544@outlook.com>
2020-05-07 17:09:04 +0800

    add tampermonkey detect

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -4,12 +4,14 @@ const webpack = require('webpack');
 
 const message = `
 // ==UserScript==
-// @name        bilibili
-// @namespace   test
-// @description 共享大会员
-// @version     0.1
+// @name         bilibili-joybook
+// @version      0.0.1
+// @description  共享大会员
+// @license      MIT
 // @include     *://*.bilibili.com/*
 // @exclude     *://passport.bilibili.com/*
+// @homepage    https://github.com/PC6live/joybook-tampermonkey
+// @supportURL  https://github.com/PC6live/joybook-tampermonkey/issues
 // @grant       GM_cookie
 // @grant       GM.cookie
 // @grant       GM_setValue
